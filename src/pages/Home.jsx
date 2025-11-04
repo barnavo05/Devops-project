44841daa36dd90428190901ca91d2b19d3b4713b
Barnavo Dey <barnavodey@Barnavos-MacBook-Air.local>
2025-11-04 20:10:17 +0530

    check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         <img src={Title} alt="Tic Tac Toe" className="mb-3 h-40 -translate-y-1/4 animate__animated animate__zoomIn animate__faster" />
         <Link to="/one-player" className='font-fredoka font-semibold hover:text-red-500'>
           <div className='bg-[#F9E9D0] py-3 w-52 text-center mb-3 rounded-2xl shadow-lg cursor-pointer hover:scale-95 hover:transition-all  hover:ease-in-out animate__animated animate__zoomIn animate__faster'>
-            One Player
+            NVMAFJA
           </div>
         </Link>
         <Link to="/two-players" className='font-fredoka font-semibold hover:text-red-500'>
